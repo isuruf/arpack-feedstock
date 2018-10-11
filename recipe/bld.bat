1a4ed711d765d6ee3e619344c06d0d645c6657b1
Julien Schueller <schueller@phimeca.com>
2018-10-11 17:40:30 +0200

    Try windows support

---
+++ recipe/bld.bat
@@ -0,0 +1,15 @@
+
+mkdir build && cd build
+
+cmake -LAH -G"NMake Makefiles"                     ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
+  -DCMAKE_Fortran_FLAGS="-Mextend"                 ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" .. 
+if errorlevel 1 exit 1
+
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+ctest --output-on-failure --timeout 100
+if errorlevel 1 exit 1
+
