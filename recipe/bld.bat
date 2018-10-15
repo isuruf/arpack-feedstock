d4903d3bdac9119d28a82b559cca446ea2f3abbb
Isuru Fernando <isuruf@gmail.com>
2018-10-15 13:15:35 -0500

    Fall back to nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,18 @@
 
 mkdir build && cd build
 
-cmake -LAH -G"Ninja"                               ^
+cmake -LAH -G"NMake Makefiles"                     ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
   -DCMAKE_Fortran_FLAGS="-Mextend -Mpreprocess"    ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" .. 
 if errorlevel 1 exit 1
 
-ninja -v
+nmake VERBOSE=1
 if errorlevel 1 exit 1
 
-ninja install
+nmake install
+if errorlevel 1 exit 1
+
+ctest --output-on-failure --timeout 100
 if errorlevel 1 exit 1
 
