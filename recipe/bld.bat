99c21e517bb2e6ae700a8497744d184cad5a7c9b
Isuru Fernando <isuruf@gmail.com>
2018-10-15 13:25:33 -0500

    Use ICB

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,8 @@ mkdir build && cd build
 cmake -LAH -G"NMake Makefiles"                     ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"           ^
   -DCMAKE_Fortran_FLAGS="-Mextend -Mpreprocess"    ^
-  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" .. 
+  -DICB=yes                                        ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
 if errorlevel 1 exit 1
 
 nmake VERBOSE=1
